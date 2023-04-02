718062aeb0402bffb6c356065df0cb2b0c5950e3
arkindd <arkindd@yandex.ru>
2023-04-02 19:31:00 +0300

    Rabotaet

--- src/main/java/org/example/RussianTextBook.java
+++ src/main/java/org/example/RussianTextBook.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class RussianTextBook implements Textbook {
+public class RussianTextBook extends Book implements Textbook {
     public RussianTextBook(String author, String title){
         System.out.println("Russian textbook has created");
     }
